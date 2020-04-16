b21d88f858ab45b76ba323623a74cea9873ce2cd
estefanofc <estefano_fc@hotmail.com>
2020-04-16 01:02:55 -0700

    Reading commands implementation

--- shell.c
+++ shell.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>>
+#include <unistd.h>
 #include <assert.h>     // assert
 #include <fcntl.h>      // O_RDWR, O_CREAT
 #include <stdbool.h>    // bool
@@ -10,6 +10,15 @@
 #include <sys/wait.h>   // wait
 #define MAX_LINE 80 /* The maximum length command*/
 
+int readline(char **buffer) {
+  size_t len;
+  int number_of_chars = getline(buffer, &len, stdin);
+  if (number_of_chars > 0) {
+    //get rid of \n
+    (*buffer)[number_of_chars - 1] = '\0';
+  }
+  return number_of_chars;
+}
 int tokenize(char *line, char **tokens) {
   char *pch;
 
@@ -25,17 +34,18 @@ int tokenize(char *line, char **tokens) {
 int main() {
   char *args[MAX_LINE / 2 + 1];/* command line arguments */
   int should_run = 1; /* flag to determine when to exit program */
-
+  char *command = (char *) malloc(MAX_LINE * sizeof(char));
   while (should_run) {
     printf("osh> ");
     fflush(stdout);
-// run only once
- should_run = 0;
-// tokenize needs a char* it can modify, so need malloc
-    char *command = (char *) malloc(MAX_LINE * sizeof(char));
-    char *sample = "ls ; sleep 10 & ls -a  ";
-    strcpy(command, sample);
-// clear out args
+    int len = readline(&command);
+    if (len <= 0)
+      break;
+    if (strcmp(command, "") == 0)
+      continue;
+    if (strcmp(command, "exit") == 0)
+      break;
+    // clear out args
     for (int i = 0; i < MAX_LINE / 2 + 1; ++i)
       args[i] = NULL;
     int num_of_tokens = tokenize(command, args);
@@ -44,13 +54,13 @@ int main() {
     char **firstcmd = (char **) malloc(MAX_LINE * sizeof(char *));
     for (int i = 0; i <= num_of_tokens; ++i)
       firstcmd[i] = NULL;
-// up to first ";" or "&"
+    // up to first ";" or "&"
     int i = 0;
     while (args[i] != NULL && strcmp(args[i], ";") != 0) {
       firstcmd[i] = args[i];
       ++i;
     }
-
+    execvp(*firstcmd, firstcmd);
     /**
     * After reading user input, the steps are:
     * (1) fork a child process using fork()
