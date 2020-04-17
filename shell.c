cccf7a3067b8f8dd9a4d42057121b2d040ec43b7
estefanofc <estefano_fc@hotmail.com>
2020-04-16 20:31:15 -0700

    Executing commands implementation

--- shell.c
+++ shell.c
@@ -32,19 +32,53 @@ int tokenize(char *line, char **tokens) {
   return num;
 }
 
-int main() {
+void runCmd(char *cmdLine) {
   enum { READ, WRITE };
   char *args[MAX_LINE / 2 + 1];/* command line arguments */
-  int should_run = 1; /* flag to determine when to exit program */
   int should_wait = 1;
-  char *cmdLine = (char *) malloc(MAX_LINE * sizeof(char));
   pid_t pid;
+  int status;
   int pipeFD[2];
   //create pipe
   if (pipe(pipeFD) < 0) {
     perror("Error in creating pipe");
     exit(EXIT_FAILURE);
   }
+  for (int i = 0; i < MAX_LINE / 2 + 1; ++i)
+    args[i] = NULL;
+  int num_of_tokens = tokenize(cmdLine, args);
+  char **currCmd = (char **) malloc(MAX_LINE * sizeof(char *));
+  for (int i = 0; i <= num_of_tokens; ++i)
+    currCmd[i] = NULL;
+  // up to first ";" or "&"
+  int i = 0;
+  while (args[i] != NULL && strcmp(args[i], ";") != 0) {
+    if (strcmp(args[i], "&") == 0) {
+      should_wait = 0;
+      break;
+    }
+    currCmd[i] = args[i];
+    i++;
+  }
+  pid = fork();
+  if (pid < 0) {
+    perror("Error during fork");
+    exit(EXIT_FAILURE);
+  }
+  if (pid == 0) {
+    if (execvp(*currCmd, currCmd) < 0)
+      printf("Could not execute command");
+    //exit(EXIT_SUCCESS);
+  } else {
+    if (should_wait)
+      wait(&status);
+    //exit(EXIT_SUCCESS);
+  }
+}
+
+int main() {
+  int should_run = 1; /* flag to determine when to exit program */
+  char *cmdLine = (char *) malloc(MAX_LINE * sizeof(char));
   while (should_run) {
     printf("osh> ");
     fflush(stdout);
@@ -55,60 +89,7 @@ int main() {
       continue;
     if (strcmp(cmdLine, "exit") == 0)
       break;
-    // clear out args
-    for (int i = 0; i < MAX_LINE / 2 + 1; ++i)
-      args[i] = NULL;
-    int num_of_tokens = tokenize(cmdLine, args);
-    char **currCmd = (char **) malloc(MAX_LINE * sizeof(char *));
-    for (int i = 0; i <= num_of_tokens; ++i)
-      currCmd[i] = NULL;
-    // up to first ";" or "&"
-    int i = 0;
-    while (args[i] != NULL && strcmp(args[i], ";") != 0) {
-      if (strcmp(args[i], "&") == 0) {
-        should_wait = 0;
-        break;
-      }
-      currCmd[i] = args[i];
-      i++;
-    }
-    int status;
-    pid = fork();
-    if(pid < 0) {
-      perror("Error during fork");
-      exit(EXIT_FAILURE);
-    }
-    if(pid == 0) {
-      //Child
-      close(pipeFD[READ]);
-      dup2(pipeFD[WRITE], 1);
-      //stdout is now child's read pipe
-      execlp("/bin/cat", "cat", "pipeexeccat.cpp", NULL);
-      if (pipeFD[0] != -1) {
-        if (dup2(pipeFD[0], STDIN_FILENO) != STDIN_FILENO) {
-          perror("dup2");
-          exit(1);
-        }
-      }
-      execvp(*currCmd, currCmd);
-      perror("execvp");
-      exit(0);
-    } else {
-      // Parent
-      int status;
-      int pid_completed = wait(&status);
-      char buf[BUF_SIZE];
-      int n = read(pipeFD[READ], buf, BUF_SIZE);
-      buf[n] = '\0';
-      for (int i = 0; buf[i] != '\0'; ++i) {
-        printf(buf[i]);
-      }
-      close(pipeFD[0]);
-      close(pipeFD[1]);
-      if (should_wait)
-        waitpid(pid, &status, 0);
-      break;
-    }
+    runCmd(cmdLine);
     /**
     * After reading user input, the steps are:
     * (1) fork a child process using fork()
